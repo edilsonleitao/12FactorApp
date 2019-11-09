dba8a48f84a8e98fc3dd184b2f5f90e766bc54f0
Edilson Leitão <edilsonleitaolima@gmail.com>
2019-11-09 16:57:39 -0300

    Configuração Loggly

--- src/server.js
+++ src/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
 const dotenv = require('dotenv');
+const winston = require('winston');
+const {Loggly} = require('winston-loggly-bulk');
 
 dotenv.config();
+
+winston.add(new Loggly({
+    token: process.env.LOGGLY_TOKEN,
+    subdomain: process.env.LOGGLY_SUBDOMAIN,
+    tags: ['Winston-NodeJS'],
+    json: true
+}));
 
 const server = express();
 
@@ -14,5 +23,7 @@ mongoose.connect(
 
 server.use(express.json());
 server.use(routes);
-
-server.listen(process.env.PORT || process.env.SERVER_PORT);
+const port = process.env.PORT || process.env.SERVER_PORT;
+server.listen(port, () => {
+  winston.log('info', `Server iniciado na porta: ${port}.`)
+});
